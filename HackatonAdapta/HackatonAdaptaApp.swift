72b11368050e91840a5db8aa745b344831a50bb8
hannahbgolds <66456237+hannahbgolds@users.noreply.github.com>
2025-07-12 11:20:29 -0300

    Add files via upload

---
+++ HackatonAdapta/HackatonAdaptaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackatonAdaptaApp.swift
+//  HackatonAdapta
+//
+//  Created by Hannah Goldstein on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct HackatonAdaptaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
